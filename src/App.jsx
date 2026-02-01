e97134cdb4db96c740ab66935039803aed96df0a
Nurislam <nurislammairamkulov@gmail.com>
2026-02-01 15:21:23 +0600

    Block2

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Block2 from "./components/block2/Block2";
+
+function App() {
+  return (
+    <div className="App">
+      <Block2 />
+    </div>
+  );
+}
+
+export default App;
